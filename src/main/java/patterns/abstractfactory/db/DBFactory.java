e1717eafff546706c83c161da2a0d7eb89e5edaf
Yaroslav <loghitechdf@gmail.com>
2025-10-15 16:54:49 +0300

    abstract factory

--- src/main/java/patterns/abstractfactory/db/DBFactory.java
+++
@@ -1,9 +0,0 @@
-package patterns.abstractfactory.db;
-
-import patterns.abstractfactory.db.objects.Connection;
-import patterns.abstractfactory.db.objects.QueryExecutor;
-
-public interface DBFactory {
-    Connection createConnection();
-    QueryExecutor createQueryExecutor();
-}
--- src/main/java/patterns/abstractfactory/ui/UIFactory.java
+++
@@ -1,9 +0,0 @@
-package patterns.abstractfactory.ui;
-
-import patterns.abstractfactory.ui.component.Button;
-import patterns.abstractfactory.ui.component.Checkbox;
-
-public interface UIFactory {
-    Button createButton();
-    Checkbox createCheckbox();
-}
